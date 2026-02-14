f2ede0fe754c1e8f6c91c7b55d263f9555cc08e5
Jonas <sariegojonas16@gmail.com>
2026-02-14 20:13:21 +0800

    Fix static root for render

--- loanprocessing/settings.py
+++ loanprocessing/settings.py
@@ -124,6 +124,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'core/static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
