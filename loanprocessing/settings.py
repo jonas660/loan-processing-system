674c33c49e3a3b8286fc4bab87d8f26e7848551e
Jonas <sariegojonas16@gmail.com>
2026-02-15 08:39:28 +0800

    Updated DATABASES config to use Render PostgreSQL

--- loanprocessing/settings.py
+++ loanprocessing/settings.py
@@ -79,11 +79,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'loanprocessing.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
 
 
 
